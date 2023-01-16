ad70544fc60ce43d6c4671832fd03dbe8d7fb391
Diego Gracilieri <root@DIEGO-GRACILIERI.local>
2023-01-15 20:55:40 -0500

    Modificar Data

--- data.tf
+++ data.tf
@@ -5,3 +5,14 @@ data "aws_cloudfront_cache_policy" "CachingDisabled" {
   name = "Managed-CachingDisabled"
 }
 
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
