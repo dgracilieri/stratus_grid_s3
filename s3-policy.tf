5acad5460614e716a9356538094de1ed162a7cc8
Diego Gracilieri <root@DIEGO-GRACILIERI.local>
2023-01-15 20:56:24 -0500

    Modificar S3-policy

--- s3-policy.tf
+++ s3-policy.tf
@@ -1,14 +1,3 @@
-data "aws_iam_policy_document" "s3_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.s3bucket.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
-    }
-  }
-}
 
 resource "aws_s3_bucket_policy" "mybucket" {
   bucket = aws_s3_bucket.s3bucket.id
